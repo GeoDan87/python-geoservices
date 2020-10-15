3fd83da34e40881def9be3d139a4a2c98da704ca
GeoDan87 <daniel.gallagher@parks.nyc.gov>
2020-10-15 00:04:31 -0400

    Started working on geoservices validation functions.

---
+++ urls.py
@@ -0,0 +1,5 @@
+base_url = 'https://geoservice.planning.nyc.gov/'
+
+svc_url = base_url + 'geoservice/geoservice.svc/'
+
+doc_url = 'https://nycplanning.github.io/Geosupport-UPG/appendices/appendix01/'
